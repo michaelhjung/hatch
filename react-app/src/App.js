e5e0cda24d2b24dec958901e9ecea3a9230dd6ce
Michael Jung <jung.michaelh@gmail.com>
2022-10-26 22:44:32 -0700

    Create notes reducer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import LoginForm from './components/Auth/LoginForm';
-import SignUpForm from './components/Auth/SignUpForm';
-import Navbar from './components/Navbar/Navbar';
 import ProtectedRoute from './components/Auth/ProtectedRoute';
-import User from './components/User';
 import { authenticate } from './store/session';
 import Splash from './components/Splash';
 import Footer from './components/Footer';
@@ -14,10 +10,6 @@ import Game from './components/Game';
 export default function App() {
     const [loaded, setLoaded] = useState(false);
     const dispatch = useDispatch();
-
-    // // testing purposes only:
-    // const sessionUser = useSelector(state => state.session.user);
-
 
     useEffect(() => {
         (async () => {
@@ -41,24 +33,6 @@ export default function App() {
                 </Route>
             </Switch>
             <Footer />
-
-            {/* <Navbar />
-            <Switch>
-                <Route path='/login' exact={true}>
-                    <LoginForm />
-                </Route>
-                <Route path='/sign-up' exact={true}>
-                    <SignUpForm />
-                </Route>
-                <ProtectedRoute path='/users/:userId' exact={true} >
-                    <User />
-                </ProtectedRoute>
-                <ProtectedRoute path='/' exact={true} >
-                    <div>
-                        <h1>My Home Page</h1>
-                    </div>
-                </ProtectedRoute>
-            </Switch> */}
         </BrowserRouter>
     );
 }
---
+++ react-app/src/store/notes.js
@@ -0,0 +1,135 @@
+import { csrfFetch } from './csrf';
+
+/* ----------------------------- ACTION TYPES: ----------------------------- */
+const READ_NOTES = '/notes/READ_NOTES';
+const CREATE_NOTE = '/notes/CREATE_NOTE';
+const UPDATE_NOTE = '/notes/UPDATE_NOTE';
+const DELETE_NOTE = '/notes/DELETE_NOTE';
+const CLEAR_DATA = '/notes/CLEAR_DATA';
+
+
+/* --------------------------------- READ: --------------------------------- */
+const _readNotes = (reviews) => ({
+    type: READ_NOTES,
+    payload: reviews
+});
+
+export const readNotes = () => async dispatch => {
+    const response = await fetch(`/api/notes/`);
+
+    if (response.ok) {
+        const userNotes = await response.json();
+        // console.log("USER NOTES DATA AFTER FETCH:", userNotes);
+        dispatch(_readNotes(userNotes));
+        return userNotes;
+    }
+}
+
+
+/* -------------------------------- CREATE: -------------------------------- */
+const _createNote = (noteData) => ({
+    type: CREATE_NOTE,
+    payload: noteData
+});
+
+export const createNote = (noteData) => async dispatch => {
+    const response = await fetch(`/api/notes/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(noteData)
+    });
+
+    if (response.ok) {
+        const newNote = await response.json();
+        // console.log("NEW NOTE DATA AFTER FETCH:", newNote);
+        dispatch(_createNote(newNote));
+        return newNote;
+    }
+}
+
+
+/* -------------------------------- UPDATE: -------------------------------- */
+const _updateNote = (noteData) => ({
+    type: UPDATE_NOTE,
+    payload: noteData
+});
+
+export const updateNote = (noteId, noteData) => async dispatch => {
+    const response = await fetch(`/api/notes/${noteId}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(noteData)
+    });
+
+    if (response.ok) {
+        const updatedNote = await response.json();
+        // console.log("UPDATED NOTE DATA AFTER FETCH:", updatedNote);
+        dispatch(_updateNote(updatedNote));
+        return updatedNote;
+    }
+}
+
+
+/* -------------------------------- DELETE: -------------------------------- */
+const _deleteNote = (noteId) => ({
+    type: DELETE_NOTE,
+    payload: noteId
+});
+
+export const deleteNote = (noteId) => async dispatch => {
+    const response = await fetch(`/api/notes/${noteId}`, {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+        const successMessage = await response.json();
+        // console.log("DELETE SUCCESS MSG AFTER FETCH:", successMessage);
+        dispatch(_deleteNote(noteId));
+        return successMessage;
+    }
+}
+
+
+/* ------------------------------ CLEAR DATA: ------------------------------ */
+export const clearData = () => ({
+    type: CLEAR_DATA
+});
+
+
+/* ---------------------------- NOTES REDUCER: ---------------------------- */
+const initialState = {};
+
+const notesReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case READ_NOTES:
+            newState = { ...state };
+            const userNotes = {};
+            action.payload.forEach(note => userNotes[note.id] = note);
+            newState = userNotes;
+            // console.log("NEWSTATE AFTER READ_NOTES ACTION:", newState);
+            return newState;
+        case CREATE_NOTE:
+            newState = { ...state };
+            newState[action.payload.id] = action.payload;
+            // console.log("NEWSTATE AFTER CREATE_NOTE ACTION:", newState);
+            return newState;
+        case UPDATE_NOTE:
+            newState = { ...state };
+            newState[action.payload.id] = action.payload;
+            // console.log("NEWSTATE AFTER UPDATE_NOTE ACTION:", newState);
+            return newState;
+        case DELETE_NOTE:
+            newState = { ...state };
+            delete newState[action.payload];
+            newState = { ...newState };
+            // console.log("NEWSTATE AFTER DELETE_NOTE ACTION:", newState);
+            return newState;
+        case CLEAR_DATA:
+            return initialState;
+        default:
+            return state;
+    }
+};
+
+export default notesReducer;
